89fd5bc9dd1f6e42287586de6d044385ff4e96da
PlamenCode <plamenggg233@gmail.com>
2023-03-06 21:02:54 +0200

    comit

--- Client Side Rendering - Exercise/05.Table-Search/solution.js
+++ Client Side Rendering - Exercise/05.Table-Search/solution.js
@@ -1,8 +1,50 @@
-function solve() {
-   document.querySelector('#searchBtn').addEventListener('click', onClick);
+import { html, render } from '../../node_modules/lit-html/lit-html.js';
 
-   function onClick() {
+document.getElementById('searchBtn').addEventListener('click', onClick);
+const body  = document.querySelector('tbody');
+const match = document.getElementById('searchField');
+
+async function onClick() {
+   const info = await getInfo();
+   const matchValue = match.value;
+   const elements = template(info, matchValue)
+
+   render(elements, body)
+
+   match.value = '';
+
+};
+
+async function getInfo(){
+   const url = 'http://localhost:3030/jsonstore/advanced/table';
+   const responce = await fetch(url);
+   const data = await responce.json();
+   return Object.values(data);
+};
+
+function template(info, match){
+   return html`
+   ${info.map(el => elTemplate(el, match))}`
+};
+
+function elTemplate(el,match = ''){
+
+   let isSelected = false;
+   const matchLower = match.toLowerCase();
+   const firstName = el.firstName.toLowerCase();
+   const lastName = el.lastName.toLowerCase();
+   const email = el.email.toLowerCase();
+   const courses = el.course.toLowerCase();
    
-
-   }
+   if(firstName.includes(matchLower) || lastName.includes(matchLower) 
+   || email.includes(matchLower) || courses.includes(matchLower)){
+      isSelected = true;
+   }   
+   
+   return html`
+<tr class = ${isSelected ? 'select' : ''}> 
+   <td>${el.firstName} ${el.lastName}</td>
+   <td>${el.email}</td>
+   <td>${el.course}</td>
+</tr>`
 }
--- Client Side Rendering - Exercise/06.Book-Library/app.js
+++ Client Side Rendering - Exercise/06.Book-Library/app.js
@@ -1,1 +1,56 @@
-console.log('My requests...')
+import { html, render } from '../../node_modules/lit-html/lit-html.js' 
+
+const loadBtn = document.getElementById('loadBooks');
+const addForm = document.getElementById('add-form');
+const editForm = document.getElementById('edit-form');
+editForm.style.display = 'none';
+
+const body = document.querySelector('tbody');
+
+loadBtn.addEventListener('click', loadBooks);
+
+async function loadBooks(ev){
+    ev.preventDefault();
+    const books = Object.entries(await getBooks());
+
+    const result = books.map(book => rowTemplate(book))
+
+    render(result, body);
+
+};
+
+function rowTemplate(book){
+    return html`
+    <tr id = ${book[0]}>
+        <td class = 'title'>${book[1].title}</td>
+        <td class = 'author'>${book[1].author}</td>
+        <td>
+            <button @click = ${onEdit}>Edit</button>
+            <button @click = ${onDel}>Delete</button>
+        </td>
+    </tr>`
+};
+
+function onEdit(ev){
+    ev.preventDefault();
+    const row = ev.target.parentElement.parentElement;
+
+    const title = row.querySelector('.title').textContent;
+    const author = row.querySelector('.author').textContent;
+    
+    addForm.style.display = 'none';
+    editForm.style.display = 'block';
+    
+};
+
+function onDel(ev){
+    ev.preventDefault();
+}
+
+
+async function getBooks(){
+    const url = 'http://localhost:3030/jsonstore/collections/books';
+    const responce = await fetch(url);
+    const data = await responce.json();
+    return data;
+}
